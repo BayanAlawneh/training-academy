5aee5715635de0ebbdd500f65858fc5c914b11b6
Bayan Alawneh <bayan44alawneh@gmail.com>
2026-07-30 10:42:08 +0300

    feat: add swagger api documentation

--- backend/src/main/java/com/academy/tms/config/SecurityConfig.java
+++ backend/src/main/java/com/academy/tms/config/SecurityConfig.java
@@ -21,7 +21,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+               .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().permitAll());
         return http.build();
     }
 }
